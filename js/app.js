77203c23a55f607aacd4b78b56f31feceb2f869b
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2023-05-28 13:31:30 -0300

    adicionado menu mobile, alteracoes css e js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,55 @@
 const copySlide = document.querySelector('.logos-slide').cloneNode(true)
 const logos = document.querySelector('.logos').appendChild(copySlide)
+
+const navMenu = document.querySelector('.nav-menu')
+const navToggle = document.querySelector('.nav-toggle')
+const navClose = document.querySelector('.nav-close')
+//mostra o menu
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+//esconde o menu
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+//obtem a referencia de todos os links do menu
+const navLink = document.querySelectorAll('.nav-link')
+
+navLink.forEach(link =>
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+)
+
+const scrollHeader = () => {
+  const navToggleButton = document.querySelector('.nav-toggle')
+  const navLogo = document.querySelector('.nav-logo')
+  const header = document.querySelector('.header')
+
+  if (this.scrollY >= 18) {
+    header.classList.add('scroll-header')
+    navToggleButton.classList.add('nav-toggle-color')
+    navLogo.classList.add('nav-logo-color')
+  } else {
+    header.classList.remove('scroll-header')
+    navToggleButton.classList.remove('nav-toggle-color')
+    navLogo.classList.remove('nav-logo-color')
+  }
+}
+document.addEventListener('scroll', scrollHeader)
+
+const baseDate = 2001
+
+const showServiceYears = year => {
+  const yearsCount = document.querySelector('[data-js="yearsCount"]')
+
+  const date = new Date()
+
+  yearsCount.textContent = date.getFullYear() - year
+}
+
+showServiceYears(baseDate)
